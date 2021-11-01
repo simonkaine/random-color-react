dc4dcdf80b8dd443d5897c9e3c1f8314408ee686
Simon Kaine <simonkaine@gmail.com>
2021-11-01 15:08:21 -0700

    Correctly renders component message + work in progress.

---
+++ src/randomColor/randomColor.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RandomColor extends Component {
+    render() { 
+        return (
+            <>
+            <h1>YO</h1>
+            </>
+        )
+    }
+}
+ 
+export default RandomColor;
